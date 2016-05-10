8f2e21dfd80b25dcd6d84dd6313568b3c2687262
climbingcode <git@climbingcode.com>
2016-05-09 22:29:45 -0700

    fixed error

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     ],
     output: {
         path: path.resolve(__dirname, "build"),
-        publicPath: "/",
+        publicPath: "/assets/",
         filename: "bundle.js"
     },
     module: defaultSettings.clientLoaders(),
