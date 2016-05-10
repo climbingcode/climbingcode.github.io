a9806117298f8e2d1f1539615da5c812cddb193e
climbingcode <git@climbingcode.com>
2016-05-09 22:20:07 -0700

    build

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     ],
     output: {
         path: path.resolve(__dirname, "build"),
-        publicPath: "/assets/",
+        publicPath: "/",
         filename: "bundle.js"
     },
     module: defaultSettings.clientLoaders(),
